8f8d0cd1b90dbdd6ece056288e4bcf5f4116e798
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-06-20 16:50:46 -0300

    Ajustando html + criando e personalizando switch

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("form")
+const choices = document.getElementById("numbers-quantity")
+const of = document.getElementById("start-range")
+const to = document.getElementById("end-range")
