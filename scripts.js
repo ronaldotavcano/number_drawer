450d64186ff4646e1661a79cc013c3f25713d154
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-06-23 15:52:58 -0300

    Iniciando o Js

--- scripts.js
+++ scripts.js
@@ -4,43 +4,65 @@ const min = document.getElementById("start-range")
 const max = document.getElementById("end-range")
 const option = document.getElementById("No-repeat")
 
-qtd.oninput = () => {
-    let value = qtd.value.replace(/\D+/g, "")
-    console.log(value)   
-}
+form.onsubmit = (event) => {
+    event.preventDefault()
 
-min.oninput = () => {
-    let minValue = min.value.replace(/\D+/g, "")
-    console.log(minValue)    
-}
-
-max.oninput = () => {
-    let maxValue = max.value.replace(/\D+/g, "")
-    console.log(maxValue)   
-}
-
-option.addEventListener("change", function(){
-    if (this.checked){
-        option = 1
-        console.log("marcado")
-    } else{
-        option = 0
-        console.log("Desmarcado")
+    const newInfo = {
+        id: new Date().getTime(),
+        qtd: parseInt(qtd.value),
+        min: parseInt(min.value),
+        max: parseInt(max.value),
+        option: option.checked, 
     }
-})
-
-function creatArray (min, max){
-    let range = []
-    const total = max - min + 1
-    for (let i = min; i < total; i++){
-        range.push(i)
-    }
+    console.log(newInfo)
+    createArray(newInfo)
 }
 
-console.log(creatArray(max))
+function createArray(info) {
+    let result = [];
 
+    if (info.qtd <= 0) {
+        alert("Selecione um valor válido");
+        return;
+    }
 
+    if (info.min > info.max) {
+        alert("Atenção: o valor inicial da sequência deve ser maior do que o valor final");
+        return;
+    }
 
+    if (info.option == true) {
+        let list = [];
 
+        for (let i = info.min; i <= info.max; i++) {
+            list.push(i);
+        }
 
+        shuffleNoRepeat(list);
+        result = list.slice(0, info.qtd);
+    } else {
+        for (let i = 0; i < info.qtd; i++) {
+            let randomNumber = Math.floor(Math.random() * (info.max - info.min + 1)) + info.min;
+            result.push(randomNumber);
+        }
+    }
 
+    // Criar nova div com conteúdo
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("resultado");
+
+    const title = document.createElement("p");
+    title.textContent = "Números sorteados:";
+    newDiv.appendChild(title);
+
+    result.forEach(number => {
+        const span = document.createElement("span");
+        span.textContent = number;
+        span.classList.add("numero-sorteado");
+        newDiv.appendChild(span);
+    });
+
+    
+    const old = document.getElementById("old");
+    old.replaceWith(newDiv);
+}
